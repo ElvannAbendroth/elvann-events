39074d8c1a0a369903930c9973e5b8268ea34447
Elvann <elvann.abendroth@gmail.com>
2023-06-25 16:02:11 +0300

    🌈 Styling Basics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="light">
       <body>{children}</body>
     </html>
   )
--- src/styles/themeSwapperPlugin.js
+++ src/styles/themeSwapperPlugin.js
@@ -4,90 +4,105 @@ const colors = require('tailwindcss/colors')
 const darkest = colors.gray[950]
 const lightest = colors.gray[100]
 
+const customEmerald = {
+  50: '#DEEAE',
+  100: '#AFD4BD',
+  200: '#89BEA0',
+  300: '#6BA988',
+  400: '#4F9373',
+  500: '#3B7D61',
+  600: '#2E6E56',
+  700: '#235F4B',
+  800: '#1B4F40',
+  900: '#134035',
+  950: '#0D312A',
+}
+
+const defaultLightTheme = {
+  name: 'base',
+  selectors: [':root', '[data-theme="light"]'],
+  theme: {
+    colors: {
+      background: lightest,
+      foreground: darkest,
+      border: colors.gray[200],
+      input: colors.gray[200],
+      ring: colors.gray[200],
+      primary: {
+        DEFAULT: customEmerald[500],
+        hover: customEmerald[400],
+        title: customEmerald[800],
+        foreground: lightest,
+      },
+      secondary: {
+        DEFAULT: colors.lime[500],
+        hover: colors.lime[400],
+        foreground: darkest,
+      },
+      danger: {
+        DEFAULT: colors.red[500],
+        hover: colors.red[400],
+        foreground: lightest,
+      },
+      success: {
+        DEFAULT: colors.teal[500],
+        hover: colors.teal[400],
+        foreground: darkest,
+      },
+      warn: {
+        DEFAULT: colors.teal[500],
+        hover: colors.teal[400],
+        foreground: lightest,
+      },
+      muted: {
+        DEFAULT: colors.gray[500],
+        hover: colors.gray[400],
+        foreground: lightest,
+      },
+      accent: {
+        DEFAULT: colors.teal[600],
+        hover: colors.teal[500],
+        foreground: lightest,
+      },
+      popover: {
+        DEFAULT: lightest,
+        foreground: darkest,
+      },
+      card: {
+        DEFAULT: lightest,
+        foreground: darkest,
+      },
+    },
+  },
+}
+const darkTheme = {
+  name: 'dark',
+  selectors: ['.dark', '[data-theme="dark"]'],
+  theme: {
+    colors: {
+      background: darkest,
+      foreground: lightest,
+      border: colors.gray[800],
+      input: colors.gray[950],
+      ring: colors.gray[800],
+      muted: {
+        DEFAULT: colors.gray[600],
+        hover: colors.gray[500],
+        foreground: lightest,
+      },
+      popover: {
+        DEFAULT: darkest,
+        foreground: lightest,
+      },
+      card: {
+        DEFAULT: darkest,
+        foreground: lightest,
+      },
+    },
+  },
+}
+
 const themeSwapperPlugin = themeSwapper({
-  themes: [
-    {
-      name: 'base',
-      selectors: [':root', '[data-theme="light"]'],
-      theme: {
-        colors: {
-          background: lightest,
-          foreground: darkest,
-          border: colors.gray[200],
-          input: colors.gray[200],
-          ring: colors.gray[200],
-          primary: {
-            DEFAULT: colors.sky[300],
-            hover: colors.sky[200],
-            foreground: darkest,
-          },
-          secondary: {
-            DEFAULT: colors.lime[500],
-            hover: colors.lime[400],
-            foreground: darkest,
-          },
-          danger: {
-            DEFAULT: colors.red[500],
-            hover: colors.red[400],
-            foreground: lightest,
-          },
-          success: {
-            DEFAULT: colors.teal[500],
-            hover: colors.teal[400],
-            foreground: darkest,
-          },
-          warn: {
-            DEFAULT: colors.teal[500],
-            hover: colors.teal[400],
-            foreground: lightest,
-          },
-          muted: {
-            DEFAULT: colors.gray[500],
-            hover: colors.gray[400],
-            foreground: lightest,
-          },
-          accent: {
-            DEFAULT: colors.teal[600],
-            hover: colors.teal[500],
-            foreground: lightest,
-          },
-          popover: {
-            DEFAULT: lightest,
-            foreground: darkest,
-          },
-          card: {
-            DEFAULT: lightest,
-            foreground: darkest,
-          },
-        },
-      },
-    },
-    {
-      name: 'dark',
-      selectors: ['.dark', '[data-theme="dark"]'],
-      theme: {
-        colors: {
-          background: darkest,
-          foreground: lightest,
-          border: colors.gray[800],
-          input: colors.gray[950],
-          ring: colors.gray[800],
-          muted: {
-            DEFAULT: colors.gray[600],
-            hover: colors.gray[500],
-            foreground: lightest,
-          },
-          popover: {
-            DEFAULT: darkest,
-            foreground: lightest,
-          },
-          card: {
-            DEFAULT: darkest,
-            foreground: lightest,
-          },
-        },
-      },
-    },
-  ],
+  themes: [defaultLightTheme, darkTheme],
 })
 exports.themeSwapperPlugin = themeSwapperPlugin
